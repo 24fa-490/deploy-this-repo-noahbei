418a7b6e9243170a8d9d5c5a1cdfa468774f5a93
Noah Beidelman <nbeidelman9@gmail.com>
2024-10-28 21:28:38 -0700

    add vercel postgres

--- src/routes/fromDBandComponents/+page.server.js
+++ src/routes/fromDBandComponents/+page.server.js
@@ -1,17 +1,40 @@
 
-import sql from '$lib/server/database';
+// import sql from '$lib/server/database';
+
+// export async function load() {
+//     const rows = await sql`
+//     SELECT
+//         containernumber,
+//         nameofship,
+//         containersize,
+//         datecontainershipped
+//     FROM
+//         containers`;
+
+//     console.log({rows});
+
+//     return { containers: rows };
+// }
+
+import POSTGRES_URL from '$lib/server/database';
+import { createPool } from '@vercel/postgres';
 
 export async function load() {
-    const rows = await sql`
-    SELECT
-        containernumber,
-        nameofship,
-        containersize,
-        datecontainershipped
-    FROM
-        containers`;
+    try{
+        const db = createPool({ connectionString: POSTGRES_URL })
+        const { rows: containers } = await db.query(
+            `SELECT 
+                containernumber,
+                nameofship,
+                containersize,
+                datecontainershipped
+            FROM 
+                containers`
+        )
+        return {
+            containers: containers
+        }
+    } catch (error) {
 
-    console.log({rows});
-
-    return { containers: rows };
+    }
 }
